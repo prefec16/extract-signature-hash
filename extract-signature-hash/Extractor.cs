1d15a9f322c56a85f4782d0b8cd1074b97aff6f4
prefec16 <prefec16@new.edvhtl.at>
2021-01-29 18:41:44 +0100

    init

---
+++ extract-signature-hash/Extractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace extract_signature_hash
+{
+    class Extractor
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input path to signed binary: ");
+            X509Certificate cert = X509Certificate.CreateFromSignedFile(Console.ReadLine());
+            Console.WriteLine(cert.GetCertHashString());
+        }
+    }
+}
